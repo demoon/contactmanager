d70a9d34ec1f443bea9aa66bded402c50e845a6e
Marcin Szymczak <marcin.marek.szymczak@gmail.com>
2019-01-17 22:03:54 +0100

    Updates

---
+++ precache-manifest.13be5fe5d239e771d5b8823bd925ddf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "b038107146551a6a45b6",
+    "url": "/contactmanager/static/js/main.b0381071.chunk.js"
+  },
+  {
+    "revision": "1cdb02f5d43b1095b92a",
+    "url": "/contactmanager/static/js/1.1cdb02f5.chunk.js"
+  },
+  {
+    "revision": "b038107146551a6a45b6",
+    "url": "/contactmanager/static/css/main.2a1c990c.chunk.css"
+  },
+  {
+    "revision": "1cdb02f5d43b1095b92a",
+    "url": "/contactmanager/static/css/1.1c9999e3.chunk.css"
+  },
+  {
+    "revision": "f9ea320685a34f281419118c9656ea67",
+    "url": "/contactmanager/index.html"
+  }
+];
